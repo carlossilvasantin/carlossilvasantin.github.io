29e3b025cb8bb0a371468d0ad49d7156bd208910
carlossilvasantin <carlossilvasantin@users.noreply.github.com>
2026-03-16 14:03:27 +0100

    Sync Telegram references from bot publishes

--- telegram-news.js
+++ telegram-news.js
@@ -1,4 +1,14 @@
 window.TELEGRAM_NEWS = {
   "channel": "admiraNext",
-  "items": []
+  "items": [
+    {
+      "id": "6",
+      "channel": "admiraNext",
+      "url": "https://t.me/admiraNext/6",
+      "published_at": "2026-03-16T13:02:56.000Z",
+      "views": "",
+      "title": "https://youtube.com/shorts/9Mzh_-LJWvM?si=Ym1aiUHsJ9I3pR7G",
+      "body": "https://youtube.com/shorts/9Mzh_-LJWvM?si=Ym1aiUHsJ9I3pR7G"
+    }
+  ]
 };
